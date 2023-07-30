aa7abd3f425766115829766a0e4e1217f7d797ea
Minh Manh <manhminh2601@gmail.com>
2023-07-30 22:02:32 +0700

    loading-bar

--- src/utils/axiosCustomize.js
+++ src/utils/axiosCustomize.js
@@ -29,6 +29,7 @@ instance.interceptors.response.use(function (response) {
 }, function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
+    NProgress.done();
     return error && error.response && error.response.data ? error.response.data : Promise.reject(error);
 });
 
