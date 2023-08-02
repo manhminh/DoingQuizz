0340b8f47f339c83247e704468e6d47e3190f270
Minh Manh <manhminh2601@gmail.com>
2023-08-02 15:18:41 +0700

    process-data

--- src/components/Users/DetailQuiz.js
+++ src/components/Users/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom"
 import { getDataQuiz } from "../../services/apiServices";
+import _ from "lodash";
 
 const DetailQuiz = (props) => {
     const param = useParams();
@@ -12,7 +13,28 @@ const DetailQuiz = (props) => {
 
     const fetchQuestions = async (quizId) => {
         let res = await getDataQuiz(quizId);
-        console.log('check res: ', res);
+        console.log('check question: ', res);
+        if (res && res.EC === 0) {
+            let raw = res.DT
+            let data = _.chain(raw)
+                // Group the elements of Array based on `id` property
+                .groupBy("id")
+                // `key` is group's name (quizId), `data` is the array of objects
+                .map((value, key) => {
+                    let questionDescription, image = null;
+                    let answers = [];
+                    value.forEach((item, index) => {
+                        if (index === 0) {
+                            questionDescription = item.description;
+                            image = item.image;
+                        }
+                        answers.push(item.answers);
+                    })
+                    return { quizId: key, data: answers, questionDescription, image }
+                })
+                .value()
+            console.log('check data:', data);
+        }
     }
 
     return (
