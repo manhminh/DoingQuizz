e11254653640bef6d14c5078b6935d85c2361801
Minh Manh <manhminh2601@gmail.com>
2023-08-05 15:51:18 +0700

    create Api create quizz

--- src/components/Admin/Content/Quiz/ManageQuiz.js
+++ src/components/Admin/Content/Quiz/ManageQuiz.js
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./ManageQuiz.scss"
 import Select from 'react-select';
+import { toast } from "react-toastify"
+import { postCreateNewQuiz } from "../../../../services/apiServices";
 
 const ManageQuiz = (props) => {
+    const imageInputRef = useRef(null);
     const options = [
         { value: 'EASY', label: 'EASY' },
         { value: 'MEDIUM', label: 'MEDIUM' },
@@ -11,11 +14,30 @@ const ManageQuiz = (props) => {
 
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
-    const [type, setType] = useState('EASY');
+    const [type, setType] = useState('');
     const [image, setImage] = useState(null);
 
     const handleChangeFile = (e) => {
+        if (e.target && e.target.files && e.target.files[0]) {
+            setImage(e.target.files[0]);
+        }
+    }
 
+    const handleSubmitQuiz = async () => {
+        if (!name || !description) {
+            toast.error('Name/Description is required')
+        }
+        let res = await postCreateNewQuiz(description, name, type?.value, image);
+        console.log('check res', res);
+        if (res && res.EC === 0) {
+            toast.success(res.EM);
+            setName('');
+            setDescription('');
+            imageInputRef.current.value = null;
+            setImage(null)
+        } else {
+            toast.error(res.EM)
+        }
     }
     return (
         <div className="quiz-container">
@@ -48,8 +70,8 @@ const ManageQuiz = (props) => {
                     </div>
                     <div className="my-3">
                         <Select
-                            value={type}
-                            // onChange={this.handleChange}
+                            defaultValue={type}
+                            onChange={setType}
                             options={options}
                             placeholder={"Quiz type..."}
                         />
@@ -57,10 +79,15 @@ const ManageQuiz = (props) => {
                     <div className="more-actions form-group">
                         <label className="mb-1">Upload Image</label>
                         <input
+                            ref={imageInputRef}
                             type="file"
                             className="form-control"
                             onChange={(e) => handleChangeFile(e)}
                         />
+                    </div>
+
+                    <div className="mt-3">
+                        <button className="btn btn-warning" onClick={() => handleSubmitQuiz()}>Save</button>
                     </div>
                 </fieldset>
             </div>
